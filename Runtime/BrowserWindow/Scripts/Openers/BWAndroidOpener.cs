db98e57f9815657f11fccc7fa4a22f050d229a68
Narek <narek.email@gmail.com>
2022-10-28 22:26:57 +0400

    Add methods for custom config
    These methods were added to the class as non-static. The static Open() method remains.

--- Runtime/BrowserWindow/Scripts/Openers/BWAndroidOpener.cs
+++ Runtime/BrowserWindow/Scripts/Openers/BWAndroidOpener.cs
@@ -1,22 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace NT.Android {
     public class BWAndroidOpener {
-        public static void Open(string url) {
+        public static void Open(string url, BWAndroidConfig config = null) {
             // Retrieve the UnityPlayer class
             AndroidJavaClass unityPlayerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
             // Retrieve the UnityPlayerActivity object (the current context)
             AndroidJavaObject unityActivity = unityPlayerClass.GetStatic<AndroidJavaObject>("currentActivity");
             // Retrieve the class from our native plugin
             AndroidJavaObject native = new AndroidJavaObject("dev.torosyan.BrowserWindow");
+            // If we have a config, apply it
+            if (config != null) {
+                ApplyConfig(config, native);
+            }
             // Call the plugin with the URL
             var args = new object[] {
                 url,
                 unityActivity
             };
             native.Call("OpenWindow", args);
+        }
+
+        private static void ApplyConfig(BWAndroidConfig config, AndroidJavaObject native) {
+            if (config.ColorCode != "") {
+                native.Call("SetColorCode", config.ColorCode);
+            }
         }
     }
 }
