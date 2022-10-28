a4f432ddf0d8a47b5346dbaf1f650caef6898776
Narek <narek.email@gmail.com>
2022-10-28 23:48:09 +0400

    Update sample scene

--- Samples~/BWSample/Scripts/BrowserWindowSample.cs
+++ Samples~/BWSample/Scripts/BrowserWindowSample.cs
@@ -1,3 +1,4 @@
+using NT.Android;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,11 +7,14 @@ namespace NT.Samples {
 
         public InputField URLField;
         public Button OpenButton;
+        public Button AdvancedSettingsOpener;
+        public Button OpenColoredButton;
 
         void Start() {
             // Add some listeners
             URLField.onValueChanged.AddListener(InputValueChanged);
             OpenButton.onClick.AddListener(Open);
+            OpenColoredButton.onClick.AddListener(OpenColored);
         }
 
         // This is the main function that calls BrowserWindow.
@@ -28,5 +32,23 @@ namespace NT.Samples {
         void InputValueChanged(string text) {
             OpenButton.interactable = text.Length > 0;
         }
+
+        #region Advanced settings
+        // This is a function that calls BrowserWindow with a config.
+        void OpenColored() {
+            // Get the text
+            string url = URLField.text;
+            // If there is no "http" in the start, add it
+            if (!url.StartsWith("http")) url = "http://" + url;
+            // Create a config
+            BWAndroidConfig androidConfig = new BWAndroidConfig();
+            androidConfig.SetColor(Color.blue);
+            // Create a BrowserWindow instance
+            BrowserWindow window = new BrowserWindow();
+            window.SetAndroidConfig(androidConfig);
+            // Open browser window
+            window.CustomOpen(url);
+        }
+        #endregion
     }
 }
