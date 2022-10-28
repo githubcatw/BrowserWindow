575aacbc0faf50cafb54d4e36d20de2a861e4972
Narek <narek.email@gmail.com>
2022-10-28 22:26:57 +0400

    Add methods for custom config
    These methods were added to the class as non-static. The static Open() method remains.

--- Runtime/BrowserWindow/Scripts/BrowserWindow.cs
+++ Runtime/BrowserWindow/Scripts/BrowserWindow.cs
@@ -5,9 +5,10 @@ namespace NT {
     /// <summary>
     /// Opens web browser windows.
     /// </summary>
-    public static class BrowserWindow {
+    public class BrowserWindow {
+        #region Open with default settings
         /// <summary>
-        /// Open the system browser with the passed URL.
+        /// Open the system browser with the passed URL and default settings.
         /// </summary>
         /// <param name="url">The URL to open.</param>
 #if UNITY_EDITOR
@@ -26,5 +27,43 @@ namespace NT {
             Application.OpenURL(url);
         }
 #endif
+        #endregion
+
+        #region Setting a custom configuration
+        /// <summary>
+        /// The configuration for Android custom tabs.
+        /// </summary>
+        private BWAndroidConfig AndroidConfig;
+        /// <summary>
+        /// Sets the configuration for Android windows.
+        /// </summary>
+        /// <param name="config">The configuration.</param>
+        public void SetAndroidConfig(BWAndroidConfig config) {
+            AndroidConfig = config;
+        }
+        #endregion
+
+        #region Open with a custom configuration
+        /// <summary>
+        /// Open the system browser with the passed URL and settings.
+        /// </summary>
+        /// <param name="url">The URL to open.</param>
+#if UNITY_EDITOR
+        // Editor - open the URL in a standard browser
+        public static void CustomOpen(string url) {
+            Application.OpenURL(url);
+        }
+#elif UNITY_ANDROID
+        // Android - open Chrome custom tab
+        public static void CustomOpen(string url) {
+            BWAndroidOpener.Open(url, AndroidConfig);
+        }
+#else
+        // Default to opening the URL in a standard browser
+        public static void CustomOpen(string url) {
+            Application.OpenURL(url);
+        }
+#endif
+        #endregion
     }
 }
