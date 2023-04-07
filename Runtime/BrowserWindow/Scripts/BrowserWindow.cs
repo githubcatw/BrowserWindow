ecd3de73ebe0eb6cda499a7b022a3dd089367f47
Narek <narek.email@gmail.com>
2023-04-07 19:35:49 +0400

    Clean up #if statements

--- Runtime/BrowserWindow/Scripts/BrowserWindow.cs
+++ Runtime/BrowserWindow/Scripts/BrowserWindow.cs
@@ -12,49 +12,39 @@ namespace NT {
         /// Open the system browser with the passed URL and default settings.
         /// </summary>
         /// <param name="url">The URL to open.</param>
+        public static void Open(string url) {
 #if UNITY_EDITOR
-        // Editor - open the URL in a standard browser
-        public static void Open(string url) {
+            // Editor - open the URL in a standard browser
             Application.OpenURL(url);
-        }
 #elif UNITY_IPHONE
-        // iOS - use SKSafariViewController
-        public static void Open(string url) {
+            // iOS - use SKSafariViewController
             BWiOSOpener.Open(url);
-        }
 #elif UNITY_ANDROID
-        // Android - open Chrome custom tab
-        public static void Open(string url) {
+            // Android - open Chrome custom tab
             BWAndroidOpener.Open(url);
-        }
 #else
-        // Default to opening the URL in a standard browser
-        public static void Open(string url) {
+            // Default to opening the URL in a standard browser
             Application.OpenURL(url);
-        }
 #endif
+        }
         #endregion
 
         #region Close
         /// <summary>
         /// If a browser window is open, close it.
         /// </summary>
+        public static void Close() {
 #if UNITY_EDITOR
-        // Editor is unsupported
-        public static void Close() {
+            // Editor is unsupported
             Debug.LogWarning("BrowserWindow.Close() is unsupported in the Editor.");
-        }
 #elif UNITY_IPHONE
-        // iOS - use SKSafariViewController
-        public static void Close() {
+            // iOS - use SKSafariViewController
             BWiOSOpener.Close();
-        }
 #else
-        // Any other platform is unsupported
-        public static void Close() {
+            // Any other platform is unsupported
             Debug.LogError("BrowserWindow.Close() is unsupported on this platform.");
-        }
 #endif
+        }
         #endregion
 
         #region Setting a custom configuration
@@ -76,22 +66,19 @@ namespace NT {
         /// Open the system browser with the passed URL and settings.
         /// </summary>
         /// <param name="url">The URL to open.</param>
+
+        public void CustomOpen(string url) {
 #if UNITY_EDITOR
-        // Editor - open the URL in a standard browser
-        public void CustomOpen(string url) {
+            // Editor - open the URL in a standard browser
             Application.OpenURL(url);
-        }
 #elif UNITY_ANDROID
-        // Android - open Chrome custom tab with Android config
-        public void CustomOpen(string url) {
+            // Android - open Chrome custom tab with Android config
             BWAndroidOpener.Open(url, AndroidConfig);
-        }
 #else
-        // Default to Open() with no parameters
-        public void CustomOpen(string url) {
+            // Default to Open() with no parameters
             Open(url);
-        }
 #endif
+        }
         #endregion
     }
 }
